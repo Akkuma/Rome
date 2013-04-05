db7e003cd239f2f4f73114ceeed7d3ae1572f6f2
Gregory Waxman <akkuma+github@gmail.com>
2013-04-05 16:43:18 -0400

    Started documentation

--- rome.js
+++ rome.js
@@ -2,6 +2,8 @@
 	function nope(){}
 	
 	function Rome() {}
+
+	//Potential for supporting multiple registries within Rome?
 	function Registry() {
 		this.componentsLookup = {};
 		this.components = [];
@@ -32,13 +34,16 @@
 	//@TODO add setter to manipulate $ if set later defineProperty
 	var $ = Rome.$ = self.$;
 
-	//@TODO support multiple mixin strategy
+	// Strategies are a minimal abstraction/transformation layer
+	// that will allow users to replace the OOBE of Rome.
 	Rome.Strategies = {
 		mixin: function (obj, mixin) {
 			mixin(obj);
 		}
 	};
 
+	// The foundation for all components provided by Rome.
+	// Allows users to provide a new foundation for functionality they want baked in.
 	Rome.Foundation = (function () {
 		var destroy = function () {
 			this.$root.remove();
@@ -50,10 +55,15 @@
 		};
 	})();
 
+	// Before you can erect a component you must plan for it.
+	// The base component is the function you want to become a Rome component.
+	// The mixins are all the functionality you want to enhance the function with.
 	Rome.plan = function (baseComponent, mixins) {
 		reg.addComponent(baseComponent);
 
 		mixins = mixins.length ? mixins || [mixins];
+
+		// We always add Rome.Foundation for common functionality in components
 		mixins.push(Rome.Foundation);
 		for	(var i = 0, len = mixins.length; i < len; i++) {
 			var mixin = mixins[i];
@@ -65,6 +75,7 @@
 			Rome.Strategies.mixin(baseComponent, mixin);
 		}
 
+		// If Rome has already been built we want to instantly erect the newly planned component
 		wasRomeBuilt && erectInstances($('[data-rome="' + baseComponent.name + '"]'));
 	};
 
@@ -102,3 +113,37 @@
 	//@TODO support AMD/Common
 	this.Rome = Rome;
 })();
+
+
+
+var circle = function () {
+	this.area
+}
+
+circle.call(Obj.prototype)
+
+var asCircle = (func () {
+	var area = function (){
+		this.radius * this.pi;
+	};
+
+	return function () {
+		this.area = area
+	}
+})()
+
+var area = function (){}
+asCircle = function () {
+	this.area = area;
+}
+
+function Circle(radius) {
+	this.radius = radius;
+}
+
+asCircle(Circle);
+
+var c = new Circle(4);
+c.area()
+
+
