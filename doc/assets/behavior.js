b126929d6c44f388f99ac7af91e5621657c94782
Gregory Waxman <akkuma+github@gmail.com>
2013-08-08 12:11:09 -0400

    Updated docs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -21,13 +21,13 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 1,
-            "title": "Rome 0.5.1",
-            "slug": "rome-051",
+            "title": "Rome 0.6.0",
+            "slug": "rome-060",
             "isFileHeader": true
           },
           "depth": 1
         },
-        "title": "Rome 0.5.1"
+        "title": "Rome 0.6.0"
       },
       "depth": 1,
       "outline": []
